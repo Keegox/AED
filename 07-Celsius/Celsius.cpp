b1f08cf16ef3a9283b8b6d511d5af63c9778c94f
Tobías Sailer Quintana <tsailerquintana@frba.utn.edu.ar>
2022-11-09 20:52:01 -0300

    ok

--- 07-Celsius/Celsius.cpp
+++ 07-Celsius/Celsius.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
+#include <cassert>
 
-float 5.0 / 9.0;
+//  Celsius : R -> R / Celsius(x) = 5/9 (x - 32)
+double Celsius(double);
+//  AreNear : R x R x R -> Boolean / AreNear(x,y,t) =   {   V   Si  |x - y| <= |t|
+//                                                  {   F   e.o.c
+bool AreNear(double, double, double = 0.00e1);
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+}
+
+double Celsius(double F)
+{
+    return (F - 32) * 5 / 9;
+}
+
+bool AreNear(double x, double y, double t)
+{
+    return abs(x - y) <= abs(t);
 }
